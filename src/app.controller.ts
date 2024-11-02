60f43b0a67e24db55e9753ca1ef40b325cd9ed1d
Moric420 <kaisermoric@gmail.com>
2024-11-02 23:42:29 +0100

    proba

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -7,4 +7,17 @@ export class AppController {
   root() {
     return {};  
   }
+  getForm() {
+    
+    const formData = {
+      name: '',
+      startDate: '',
+      endDate: '',
+      isPaidLeave: false,
+      employeeId: '',
+      justification: ''
+    };
+    console.log('getForm called, returning formData:', formData);
+    return { formData };
+  }
 }
